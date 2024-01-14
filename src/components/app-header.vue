95bfdb440155d025197a23ac76bab7c92cd79215
itzhake <itzikil@icloud.com>
2024-01-14 22:13:24 +0200

    1

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,10 +1,24 @@
 <template>
-    <section class="header flex space align-center">
-        <img src="../assets/svgs/logo.svg" alt="">
-        <div class="flex">
-            <a href="#features">Features</a>
-            <a href="#team">Team</a>
-            <a href="#sign in">Sign In</a>
+    <section class="header flex column ">
+        <div class="flex space align-center">
+            <img src="../assets/svgs/logo.svg" alt="">
+            <div class="flex">
+                <a href="#features">Features</a>
+                <a href="#team">Team</a>
+                <a href="#sign in">Sign In</a>
+            </div>
         </div>
+        <a href="https://res.cloudinary.com/dtgejpwv9/image/upload/v1705262009/preview_nqjanp.png" target="_blank"
+            class="checkout">Check out the original image</a>
     </section>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            // Replace 'your-photo.jpg' with the actual filename of your photo in the public directory
+        };
+    }
+};
+</script>
